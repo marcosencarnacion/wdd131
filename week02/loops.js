1e4e9264f3bf06b07a428aa7bc45ce78cde8e90f
marcosencarnacion <enc24001@byui.edu>
2025-01-15 20:17:28 -0500

    Select the <ul> where the list items will be added

---
+++ week02/loops.js
@@ -0,0 +1,30 @@
+// loops.js
+myInfo = {
+    name: "Brother T",
+    photo: "images/photo.jpg",
+    favoriteFoods: ["Fettucini", "Steak", "Chicken", "Shrimp", "Baked Potato"],
+    hobbies: ["Reading", "Fishing", "Camping"],
+    placesLived: [
+        {
+            place: "Rexburg, ID",
+            length: "5 years",
+        },
+        {
+            place: "Ammon, ID",
+            length: "3 years",
+        },
+        {
+            place: "Sandy, UT",
+            length: "1 year",
+        },
+    ],
+};
+
+
+const foodsElement = document.querySelector('#favorite-foods'); // Select the <ul> where the list items will be added
+
+// Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
+document.querySelector("#favorite-foods").appendChild(favoriteFood1);
+document.querySelector("#favorite-foods").appendChild(favoriteFood2);
+document.querySelector("#favorite-foods").appendChild(favoriteFood3);
+document.querySelector("#favorite-foods").appendChild(favoriteFood4);
