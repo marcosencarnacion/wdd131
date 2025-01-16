181c5fb951a724bc79dd8783322889edd5b6c69b
marcosencarnacion <enc24001@byui.edu>
2025-01-15 20:19:25 -0500

    Add the food's name

--- week02/loops.js
+++ week02/loops.js
@@ -24,6 +24,7 @@ myInfo = {
 const foodsElement = document.querySelector('#favorite-foods'); // Select the <ul> where the list items will be added
 function createAppendFoodItem(food) {
     let favoriteFood = document.createElement("li"); // Create an <li>
+    favoriteFood.textContent = food; // Add the food's name
 }
 
 // Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
