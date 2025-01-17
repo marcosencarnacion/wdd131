b617fc2410802b3c46e820de14683ce860e633ab
marcosencarnacion <enc24001@byui.edu>
2025-01-16 21:43:29 -0500

    hamburger menu

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,16 @@
+const nav = document.querySelector("#nav");
+const open = document.querySelector("#open");
+const close = document.querySelector("#close");
+
+open.addEventListener("click", () => {
+    nav.classList.add("nav-visible");
+});
+
+close.addEventListener("click", () => {
+    nav.classList.remove("nav-visible");
+});
+
+
 document.getElementById('currentyear').textContent = new Date().getFullYear();
 
 document.getElementById('lastModified').textContent = 'Last modified: ' + document.lastModified;
