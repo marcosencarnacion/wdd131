d7aec9e92ee16580240fbd7a2d3796f3c623e5c7
marcosencarnacion <enc24001@byui.edu>
2025-01-30 21:39:43 -0500

    footer with year and copy right added. Hamburger menu functionality added

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -65,3 +65,28 @@ const temples = [
 
     // Temple Data Resource: https://churchofjesuschristtemples.org/temples/
 ];
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Footer: Display the current year and the last modified date
+    const currentYear = new Date().getFullYear();
+    document.getElementById('currentyear').textContent = currentYear;
+
+    const lastModified = document.lastModified;
+    document.getElementById('lastModified').textContent = `Last updated: ${lastModified}`;
+
+    // Hambuerger Menu functionality
+    const nav = document.querySelector("#nav");
+    const open = document.querySelector("#open");
+    const close = document.querySelector("#close");
+
+    open.addEventListener("click", () => {
+        nav.classList.add("nav-visible");
+    });
+
+    close.addEventListener("click", () => {
+        nav.classList.remove("nav-visible");
+    });
+
+
+
+})
