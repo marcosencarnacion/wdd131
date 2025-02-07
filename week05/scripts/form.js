435dc0657ef88115cfb212469c11ff11fb497faf
marcosencarnacion <enc24001@byui.edu>
2025-02-06 21:15:32 -0500

    added the written review, your name, and the post my review button.

--- week05/scripts/form.js
+++ week05/scripts/form.js
@@ -35,3 +35,12 @@ products.forEach(product => {
     option.textContent = product.name;
     selectElement.appendChild(option);
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Footer: Display the current year and the last modified date
+    const currentYear = new Date().getFullYear();
+    document.getElementById('currentyear').textContent = currentYear;
+
+    const lastModified = document.lastModified;
+    document.getElementById('lastModified').textContent = `Last updated: ${lastModified}`;
+});
